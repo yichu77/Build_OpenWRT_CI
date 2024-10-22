8a513b11ca528c6776c251537c777dcb6c343c44
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-10-22 16:23:35 +0800

    更新 Diy.sh

--- hiwifi_c526a/Scripts/Diy.sh
+++ hiwifi_c526a/Scripts/Diy.sh
@@ -2,6 +2,6 @@
 
 #加回Hiwifi c526a支持
 if [[ $WRT_URL == *"lede"* && $WRT_TARGET == "ipq40xx" ]]; then
-        chmod -R 755 ./diy
-	cp -arf ./diy/* ./wrt/
+        chmod -R 755 $WRT_DIY
+	cp -arf $WRT_DIY/* ./wrt/
 fi
