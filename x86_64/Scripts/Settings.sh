94d692895201243f15440f1216aea22b4a2e2826
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-10-24 21:54:04 +0800

    更新 Settings.sh

--- x86_64/Scripts/Settings.sh
+++ x86_64/Scripts/Settings.sh
@@ -5,7 +5,7 @@ sed -i '2 i \#' feeds.conf.default
 sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
 
 # 使用源码自带ShadowSocksR Plus+出国软件
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # feeds获取theme-opentopd源码：
 sed -i '$a src-git opentopd https://github.com/sirpdboy/luci-theme-opentopd' feeds.conf.default
@@ -26,9 +26,6 @@ sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 sed -i "s/timezone='.*'/timezone='CST-8'/g" $CFG_FILE
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
 if [[ $WRT_URL == *"lede"* ]]; then
 	LEDE_FILE=$(find ./package/lean/autocore/ -type f -name "index.htm")
 	#修改默认时间格式
@@ -37,6 +34,8 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_REPO-$WRT_DATE/" $LEDE_FILE
 	#修改默认WIFI名
 	sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+        # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+        sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	#添加编译日期标识
 	VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
