037b6ff55b27a85d0d27045e7c7629120d3beb54
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-10-24 09:50:39 +0800

    更新 Settings.sh

--- x86_64/Scripts/Settings.sh
+++ x86_64/Scripts/Settings.sh
@@ -36,8 +36,8 @@ elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 fi
 
 #配置文件修改
-echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+#echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+#echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 if [[ $WRT_URL == *"lede"* ]]; then
 	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
