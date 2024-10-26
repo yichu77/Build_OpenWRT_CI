41f69ddb5582d1b923f3627eb653ba05c8507c43
openwrt <48500475+yichu77@users.noreply.github.com>
2024-10-26 14:39:52 +0800

    Update Settings.sh

--- x86_64/Scripts/Settings.sh
+++ x86_64/Scripts/Settings.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-#注释第2行
-#sed -i '2 i \#' feeds.conf.default
-sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
-
-# 使用源码自带ShadowSocksR Plus+出国软件
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
-# feeds获取theme-opentopd源码：
-#sed -i '$a src-git opentopd https://github.com/sirpdboy/luci-theme-opentopd' feeds.conf.default
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-theme-opentopd #主题
-echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
+	#注释第2行
+	#sed -i '2 i \#' feeds.conf.default
+	sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
+	# 使用源码自带ShadowSocksR Plus+出国软件
+	sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+	#修改默认IP地址
+	#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
+	sed -i 's/192.168.1.1/192.168.68.1/g' ./package/base-files/files/bin/config_generate
+fi
 
 #修改默认主题
 
 #sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
-#修改默认IP地址
-#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.68.1/g' ./package/base-files/files/bin/config_generate
+
 
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
