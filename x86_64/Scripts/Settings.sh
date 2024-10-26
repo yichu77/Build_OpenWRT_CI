3bfee39a5e28546d87d6406dbf99d312b6f2d5e8
openwrt <48500475+yichu77@users.noreply.github.com>
2024-10-26 13:58:11 +0800

    Update Settings.sh

--- x86_64/Scripts/Settings.sh
+++ x86_64/Scripts/Settings.sh
@@ -19,7 +19,7 @@ echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
 #sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.68.1/g' ./package/base-files/files/bin/config_generate
 
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
