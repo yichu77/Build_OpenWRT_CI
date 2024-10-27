23d93b6d051e7c7fe1cf06b3cffb6f961de34549
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-10-27 19:48:42 +0800

    更新 Settings.sh

--- hiwifi_c526a/Scripts/Settings.sh
+++ hiwifi_c526a/Scripts/Settings.sh
@@ -6,11 +6,7 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	#sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
 	# 使用源码自带ShadowSocksR Plus+出国软件
 	#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-	#修改默认IP地址
-	#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
- 
-	#sed -i 's/192.168.1.1/192.168.68.1/g' ./package/base-files/files/bin/config_generate
- 
+
 	# feeds获取theme-opentopd源码：
 	#sed -i '$a src-git opentopd https://github.com/sirpdboy/luci-theme-opentopd' feeds.conf.default
 	#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-theme-opentopd #主题
@@ -18,9 +14,7 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	#修改默认主题
 	#sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
-	CFG_FILE="./package/base-files/files/bin/config_generate"
-	#修改默认IP地址
-	#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
+
 	#sed -i 's/192.168.1.1/$WRT_IP/g' ./package/base-files/files/bin/config_generate
 	#修改默认主机名
 	#sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
@@ -29,8 +23,12 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	#sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 fi
 
+	CFG_FILE="./package/base-files/files/bin/config_generate"
+	#修改默认IP地址
+	#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 
-
+        # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+        sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 if [[ $WRT_URL == *"lede"* ]]; then
 	LEDE_FILE=$(find ./package/lean/autocore/ -type f -name "index.htm")
@@ -40,8 +38,7 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_REPO-$WRT_DATE/" $LEDE_FILE
 	#修改默认WIFI名
 	sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
-        # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-        sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	#添加编译日期标识
 	VER_FILE=$(find ./feeds/luci/modules/ -type f -name "10_system.js")
