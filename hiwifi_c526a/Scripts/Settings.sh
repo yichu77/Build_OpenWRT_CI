5911f8117878148cde67905fe62f55909c7f0a8d
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-10-27 20:29:02 +0800

    更新 Settings.sh

--- hiwifi_c526a/Scripts/Settings.sh
+++ hiwifi_c526a/Scripts/Settings.sh
@@ -14,7 +14,6 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	#修改默认主题
 	#sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
-
 	#sed -i 's/192.168.1.1/$WRT_IP/g' ./package/base-files/files/bin/config_generate
 	#修改默认主机名
 	#sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
