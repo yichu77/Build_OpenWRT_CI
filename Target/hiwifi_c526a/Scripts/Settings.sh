b026b7dd9cfb3c0925748d64fce13fb73a9e94e9
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-10-30 21:27:08 +0800

    更新 Settings.sh

--- Target/hiwifi_c526a/Scripts/Settings.sh
+++ Target/hiwifi_c526a/Scripts/Settings.sh
@@ -15,6 +15,8 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	#sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 	CFG_FILE="./package/base-files/files/bin/config_generate"
 	#sed -i 's/192.168.1.1/$WRT_IP/g' ./package/base-files/files/bin/config_generate
+
+E:\Github\lede\package\base-files\image-config.in
 	#修改默认主机名
 	#sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 	#修改默认时区
@@ -22,9 +24,12 @@ if [[ $WRT_URL == *"lede"* ]]; then
 	#sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" $CFG_FILE
 fi
 
-	CFG_FILE="./package/base-files/files/bin/config_generate"
+
 	#修改默认IP地址
-	#sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+	sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
+        sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package\base-files\image-config.in
+        sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package\base-files\Makefile
+
 
         # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
         sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
