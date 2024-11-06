ce23d2cf3a05278594c4af7de85968f8a7462683
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-11-06 11:09:50 +0800

    更新 Settings.sh

--- Target/hiwifi_c526a/Scripts/Settings.sh
+++ Target/hiwifi_c526a/Scripts/Settings.sh
@@ -14,17 +14,16 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 
 fi
 
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" package/lean/default-settings/files/zzz-default-settings
 
-	#修改默认主机名
-	sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" package/lean/default-settings/files/zzz-default-settings
+#修改默认IP地址
+sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/image-config.in
+sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/Makefile
 
-	#修改默认IP地址
-	sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
-        sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/image-config.in
-        sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/Makefile
-
-        # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-        sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 
 if [[ $WRT_URL == *"lede"* ]]; then
