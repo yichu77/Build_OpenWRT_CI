b97845aa3f4790add22bf79daf27dc08ac1fd8e3
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-11-07 21:06:32 +0800

    更新 Settings.sh

--- Target/hiwifi_c526a/Scripts/Settings.sh
+++ Target/hiwifi_c526a/Scripts/Settings.sh
@@ -7,23 +7,29 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	# 使用源码自带ShadowSocksR Plus+出国软件
 	sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
+	# 使用自定义package
+	sed -i 's/coolsnowwolf/yichu77/g' feeds.conf.default
+
 	# feeds获取theme-opentopd源码：
 	#sed -i '$a src-git opentopd https://github.com/sirpdboy/luci-theme-opentopd' feeds.conf.default
 	#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-theme-opentopd #主题
 	#echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+        #修改默认主机名
+        sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" package/lean/default-settings/files/zzz-default-settings
+
+	#修改默认IP地址
+	sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
+	sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/image-config.in
+	sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/Makefile
+
+	# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+	sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 fi
 
-#修改默认主机名
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" package/lean/default-settings/files/zzz-default-settings
 
-#修改默认IP地址
-sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/image-config.in
-sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/Makefile
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
 
 
 if [[ $WRT_URL == *"lede"* ]]; then
