243507d899357d452a5d5952a3cadb7db68d8baa
yichu77 <48500475+yichu77@users.noreply.github.com>
2024-11-09 11:07:30 +0800

    更新 Settings.sh

--- Target/hiwifi_c526a/Scripts/Settings.sh
+++ Target/hiwifi_c526a/Scripts/Settings.sh
@@ -3,12 +3,12 @@
 if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	#注释第2行
 	#sed -i '2 i \#' feeds.conf.default
-	sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
+	#sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
 	# 使用源码自带ShadowSocksR Plus+出国软件
 	sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 	# 使用自定义package
-	sed -i 's/coolsnowwolf/yichu77/g' feeds.conf.default
+	#sed -i 's/coolsnowwolf/yichu77/g' feeds.conf.default
 
 	# feeds获取theme-opentopd源码：
 	#sed -i '$a src-git opentopd https://github.com/sirpdboy/luci-theme-opentopd' feeds.conf.default
