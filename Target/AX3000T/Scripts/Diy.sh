512af0c57bc0bdbcc9f7e65a4d87e880fd433549
yichu77 <48500475+yichu77@users.noreply.github.com>
2025-07-26 19:40:57 +0800

    批量

--- Target/AX3000T/Scripts/Diy.sh
+++ Target/AX3000T/Scripts/Diy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+#添加DIY
+if [[ $WRT_URL == *"coolsnowwolf"* && $WRT_TARGET == "Mediatek" ]]; then
+        chmod -R 755 $WRT_DIY
+	cp -arf $WRT_DIY/* ./wrt/
+fi
--- Target/AX3000T/Scripts/Settings.sh
+++ Target/AX3000T/Scripts/Settings.sh
@@ -5,17 +5,7 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	#sed -i '2 i \#' feeds.conf.default
 	#sed -i 's/.git;openwrt-23.05//g' feeds.conf.default
 	# 使用源码自带ShadowSocksR Plus+出国软件
-	sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-
-	# 使用自定义package
-	#sed -i 's/coolsnowwolf/yichu77/g' feeds.conf.default
-
-	# feeds获取theme-opentopd源码：
-	#sed -i '$a src-git opentopd https://github.com/sirpdboy/luci-theme-opentopd' feeds.conf.default
-	#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/lean/luci-theme-opentopd #主题
-	#echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-        #修改默认主机名
-        sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" package/lean/default-settings/files/zzz-default-settings
+	#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 	#修改默认IP地址
 	sed -i "s/192\.168\.[0-9]*/$WRT_IP/g" package/base-files/files/bin/config_generate
@@ -25,22 +15,14 @@ if [[ $WRT_URL == *"coolsnowwolf"* ]]; then
 	# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 	sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
-fi
+	#修改默认主机名
+	sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
-
-
-
-
-
-if [[ $WRT_URL == *"lede"* ]]; then
 	LEDE_FILE=$(find ./package/lean/autocore/ -type f -name "index.htm")
 	#修改默认时间格式
 	sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $LEDE_FILE
 	#添加编译日期标识
 	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/\1 \/ $WRT_REPO-$WRT_DATE/" $LEDE_FILE
-fi
-
-if [[ $WRT_TARGET == "ipq40xx"  ]]; then
 	#修改默认WIFI名
 	sed -i "s/ssid=.*/ssid=$WRT_WIFI/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
